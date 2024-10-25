213df7a101c3104fee7fa3423ed1e758360e66fc
habibarezq <habibarezq30@gmail.com>
2024-10-26 00:16:34 +0300

    Some Minor Adjustments to code

--- GymManagmentSystem/src/gymmanagmentsystem/AdminRole.java
+++ GymManagmentSystem/src/gymmanagmentsystem/AdminRole.java
@@ -4,41 +4,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class AdminRole {
-  private TrainerDatabase dataBase;
 
-    public AdminRole()
-    {
-        
+    private TrainerDatabase dataBase = new TrainerDatabase("Trainers");
+
+    public AdminRole() {
+
     }
-    
-    void addTrainer(String trainerId, String name, String email, String speciality, String phoneNumber)
-    {
-        boolean flag= true; 
-        for(int i=0; i<dataBase.records.size(); i++)
-            if(dataBase.contains(trainerId))
-                flag=false;
-        if(flag) 
-        {
+
+    public void addTrainer(String trainerId, String name, String email, String speciality, String phoneNumber) {
+        boolean flag = true;
+        for (int i = 0; i < dataBase.returnAllRecords().size(); i++) {
+            if (dataBase.contains(trainerId)) {
+                flag = false;
+            }
+        }
+        if (flag) {
             Trainer newTrainer = new Trainer(trainerId, name, email, speciality, phoneNumber);
             dataBase.insertRecord(newTrainer);
         }
     }
-    ArrayList<Trainer> getListOfTrainers ()
-    {
+
+    public ArrayList<Trainer> getListOfTrainers() {
         return dataBase.returnAllRecords();
     }
-    void removeTrainer (String key)
-    {
-       int indexRecord=-1;
-        for(int i=0; i<dataBase.records.size(); i++)
-            if(dataBase.contains(key))
-                indexRecord=i;
-        if(indexRecord>0) dataBase.records.remove(indexRecord);
-        else System.out.println("This Trianer Does Not Exist.");
+
+    public void removeTrainer(String key) {
+        int indexRecord = -1;
+        for (int i = 0; i < dataBase.returnAllRecords().size(); i++) {
+            if (dataBase.contains(key)) {
+                indexRecord = i;
+            }
+        }
+        if (indexRecord > 0) {
+            dataBase.returnAllRecords().remove(indexRecord);
+        } else {
+            System.out.println("This Trianer Does Not Exist.");
+        }
     }
-    void logout () throws IOException
-    {
-      dataBase.saveToFile();
-      //arg3 lel menu tany aw a-logout somehow
+
+    public void logout() throws IOException {
+        dataBase.saveToFile();
+        //arg3 lel menu tany aw a-logout somehow
     }
 }
