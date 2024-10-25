c9f375850cdc770276b175c36fd4f90831d3a89c
habibarezq <habibarezq30@gmail.com>
2024-10-26 00:15:44 +0300

    Added MemberClassRegistration class

---
+++ GymManagmentSystem/src/gymmanagmentsystem/MemberClassRegistration.java
@@ -0,0 +1,47 @@
+
+package gymmanagmentsystem;
+
+import java.time.LocalDate;
+
+public class MemberClassRegistration implements Database {
+    //This class represents a member's 
+    //registration for a class and handles the registration data.
+    private String memberId;
+    private String classId;
+    private String status;
+    private LocalDate  registrationDate;
+
+    public MemberClassRegistration(String memberId, String classId, String status, LocalDate registrationDate) {
+        this.memberId = memberId;
+        this.classId = classId;
+        this.status = status;
+        this.registrationDate = registrationDate;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+   
+     @Override
+    public String lineRepresentation()
+   {
+       String s = String.join(",", memberId,classId)+","+registrationDate+","+status;
+       return s;
+   }
+
+    @Override
+   public String getSearchKey()
+   {
+       String s = String.join(",", memberId,classId);
+       return s;
+   } 
+    
+}
