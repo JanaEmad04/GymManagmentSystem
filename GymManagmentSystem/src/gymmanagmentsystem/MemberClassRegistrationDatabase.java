be11b3f036be98615ae06f1d0009f347ef1a7045
habibarezq <habibarezq30@gmail.com>
2024-10-29 18:55:28 +0300

    Replaced the Database from interface to Abstract

--- GymManagmentSystem/src/gymmanagmentsystem/MemberClassRegistrationDatabase.java
+++ GymManagmentSystem/src/gymmanagmentsystem/MemberClassRegistrationDatabase.java
@@ -1,52 +1,16 @@
 
 package gymmanagmentsystem;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class  MemberClassRegistrationDatabase implements Database<MemberClassRegistration> {
 
-    private String fileName;
-    private ArrayList<MemberClassRegistration> records;
+public class  MemberClassRegistrationDatabase extends Database<MemberClassRegistration> {
+
 
     public MemberClassRegistrationDatabase(String fileName) {
-        this.fileName = fileName+".txt";
-        this.records = new ArrayList<>();
+        super(fileName);
     }
-
-    @Override
-    public void readFromFile() //read men el file w b3den y3mel trainer objects w b3deen y-store fel record list
-    {File f = new File(fileName);
-        try //read men el file w b3den y3mel trainer objects w b3deen y-store fel record list
-        {
-            f.createNewFile();
-            //Read File
-             Scanner s = new Scanner(f); //NOTE CreateNewFile idea to handle if in the start the file doesn't exist instead of creating it manually odam el mo3eed
-            ArrayList<String> MemberClassInfo = new ArrayList<>();
-           
-            while (s.hasNextLine()) {
-                MemberClassInfo.add(s.nextLine());
-            }
-            for (int i = 0; i < MemberClassInfo.size(); i++) {
-                MemberClassRegistration newMemberRegister = createRecordFrom(MemberClassInfo.get(i));
-                insertRecord(newMemberRegister);
-            }
-            s.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(MemberClassRegistrationDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(MemberClassRegistrationDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
     @Override
     public MemberClassRegistration createRecordFrom(String line) {
         String[] separatedStr = line.split(",");
@@ -57,88 +21,5 @@ public class  MemberClassRegistrationDatabase implements Database<MemberClassReg
         MemberClassRegistration newMemberRegister = new MemberClassRegistration(memeberId, classId, status, registrationDate);
         return newMemberRegister;
     }
-
-    @Override
-    public ArrayList<MemberClassRegistration> returnAllRecords() {
-        return records;
-    }
-
-    @Override
-    public boolean contains(String key)  //Note that key is formatted memberID,classID
-    {
-        if(records.isEmpty())
-            return false;
-        else
-        {
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getSearchKey().equals(key))
-            return true;
-        }
-        return false;
-        }
-    }
-
-    @Override
-    public MemberClassRegistration getRecord(String key) {
-        int indexRecord=-1;
-        boolean flag = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getSearchKey().equals(key)) {
-                indexRecord = i;
-                flag = true;
-                break;
-            }
-        }
-        if (flag) {
-            return records.get(indexRecord);
-        } else {
-            return null;
-        }
-
-    }
-
-    public void insertRecord(MemberClassRegistration record) {
-        if (!contains(record.getSearchKey())) {
-            records.add(record);
-        }
-    }
-
-    @Override
-    public void deleteRecord(String key) {
-        int indexRecord = 0;
-        boolean flag = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getSearchKey().equals(key)) {
-                indexRecord = i;
-                flag = true;
-                break;
-            }
-        }
-        if (flag) {
-            records.remove(indexRecord);
-        }
-        
-    }
-
-    @Override
-    public void saveToFile() {
-        FileWriter w = null;
-        try {
-            w = new FileWriter(fileName);
-            for (int i = 0; i < records.size(); i++) {
-                w.write(records.get(i).lineRepresentation() + "\n");
-            }   w.flush();
-            w.close();
-        } catch (IOException ex) {
-            Logger.getLogger(MemberClassRegistrationDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                w.close();
-            } catch (IOException ex) {
-                Logger.getLogger(MemberClassRegistrationDatabase.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
 }
 
