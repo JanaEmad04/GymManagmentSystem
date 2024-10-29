c4cfebc0cf33bae7125ebc0b045ee911acfc88cf
habibarezq <habibarezq30@gmail.com>
2024-10-29 18:55:53 +0300

    Replaced the Database from interface to Abstract

--- GymManagmentSystem/src/gymmanagmentsystem/ClassDatabase.java
+++ GymManagmentSystem/src/gymmanagmentsystem/ClassDatabase.java
@@ -1,52 +1,13 @@
 package gymmanagmentsystem;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class ClassDatabase implements Database<Class> {
-
-    private String fileName;
-    private ArrayList<Class> records;
+public class ClassDatabase extends Database<Class> {
 
     public ClassDatabase(String fileName) {
-        this.fileName = fileName + ".txt";
-        this.records = new ArrayList<>();
+        super(fileName);
     }
 
-    @Override
-    public void readFromFile() //read men el file w b3den y3mel trainer objects w b3deen y-store fel record list
-    {
-        File f = new File(fileName);
-        try //read men el file w b3den y3mel trainer objects w b3deen y-store fel record list
-        {
-
-            f.createNewFile();
-
-            //Read File
-            Scanner s = new Scanner(f); //NOTE CreateNewFile idea to handle if in the start the file doesn't exist instead of creating it manually odam el mo3eed
-            ArrayList<String> classesInfo = new ArrayList<>();
-            while (s.hasNextLine()) {
-                classesInfo.add(s.nextLine());
-            }
-            for (int i = 0; i < classesInfo.size(); i++) {
-                Class gymClass = createRecordFrom(classesInfo.get(i));
-                insertRecord(gymClass);
-            }
-            s.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ClassDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ClassDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
+   @Override
     public Class createRecordFrom(String line) {
         String[] separatedStr = line.split(",");
         String Id = separatedStr[0].trim();
@@ -56,88 +17,6 @@ public class ClassDatabase implements Database<Class> {
         int availableSeats = Integer.parseInt(separatedStr[4].trim()); //Converted to int
         Class newClass = new Class(Id, name, trainerId, duration, availableSeats);
         return newClass;
-    }
-
-    @Override
-    public ArrayList<Class> returnAllRecords() {
-        return records;
-    }
-
-    @Override
-    public boolean contains(String key) {
-        if (records.isEmpty()) {
-            return false;
-        } else {
-            for (int i = 0; i < records.size(); i++) {
-                if (records.get(i).getSearchKey().equals(key)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
-    @Override
-    public Class getRecord(String key) {
-        int indexRecord = 0;
-        boolean flag = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getSearchKey().equals(key)) {
-                indexRecord = i;
-                flag = true;
-                break;
-            }
-        }
-        if (flag) {
-            return records.get(indexRecord);
-        } else {
-            return null;
-        }
-
-    }
-
-    public void insertRecord(Class record) {
-        if (!contains(record.getSearchKey())) {
-            records.add(record);
-        }
-
-    }
-
-    @Override
-    public void deleteRecord(String key) {
-        int indexRecord = 0;
-        boolean flag = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getSearchKey().equals(key)) {
-                indexRecord = i;
-                flag = true;
-                break;
-            }
-        }
-        if (flag) {
-            records.remove(indexRecord);
-        }
-    }
-
-    @Override
-    public void saveToFile() {
-        FileWriter w = null;
-        try {
-            w = new FileWriter(fileName);
-            for (int i = 0; i < records.size(); i++) {
-                w.write(records.get(i).lineRepresentation() + "\n");
-            }
-            w.flush();
-            w.close();
-        } catch (IOException ex) {
-            Logger.getLogger(ClassDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                w.close();
-            } catch (IOException ex) {
-                Logger.getLogger(ClassDatabase.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
     }
 
 }
